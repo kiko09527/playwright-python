30eb286754767854f5091387e1ee2cbfeeb0596e
kiko9527 <kiko9527@example.com>
2025-06-04 19:18:59 +0800

    修改页面

--- config/db_config.py
+++ config/db_config.py
@@ -1,11 +1,11 @@
-db_config = {
+db_config_local = {
     'host': 'common_01_test_ms_mysql.gaojihealth.cn',  # 数据库主机
     'user': 'scrmrw',  # 数据库用户名
     'password': '123qaz!@#',  # 数据库密码
     'database': 'autotest'  # 数据库名称
 }
 
-db_config_local = {
+db_config = {
     'host': '127.0.0.1',  # 数据库主机1
     'user': 'root',  # 数据库用户名
     'password': 'password',  # 数据库密码
